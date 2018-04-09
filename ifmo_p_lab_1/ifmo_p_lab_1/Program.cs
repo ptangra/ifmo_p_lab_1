b9ab79a0cb19dfbd709de74c8847b900a5529554
Mirat Rafikov <miratworking@gmail.com>
2018-04-09 23:57:04 +0300

    Visual Studio empty project added

---
+++ ifmo_p_lab_1/ifmo_p_lab_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifmo_p_lab_1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            }
+        }
+    }
